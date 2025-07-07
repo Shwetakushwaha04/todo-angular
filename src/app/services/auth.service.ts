2e92c6f69c9bff3d579c416d467108ed4e2fac19
byfynx <79015776+byfynx@users.noreply.github.com>
2025-07-07 06:26:02 +0000

    Session Four

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
@@ -7,9 +8,9 @@ import { catchError, map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8000';  // Change if different
+  private apiUrl = 'http://127.0.0.1:8001';  // Change if different
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   // REGISTER
   register(data:{email: string, password: string}): Observable<boolean> {
@@ -22,11 +23,13 @@ export class AuthService {
 
   // LOGIN
   login(data:{email: string, password: string}): Observable<boolean> {
-    console.log('two')
-    const payload = { email:data.email, password:data.password };
-    return this.http.post(`${this.apiUrl}/login`, payload).pipe(
+    let formData = new FormData();
+    formData.append('username',data.email);
+    formData.append('password',data.password);
+    
+    return this.http.post(`${this.apiUrl}/token`, formData).pipe(
       map((res: any) => {
-        localStorage.setItem('token', res.token);
+        localStorage.setItem('token', res.access_token);
         return true;
       }),
       catchError(() => of(false))
@@ -35,6 +38,7 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem('token');
+    this.router.navigate(['/login']);
   }
 
   isAuthenticated(): boolean {
